a99f3373955ab51eab110c5035e247a0ed09fff9
Joan Charmant <jcharmant@gmail.com>
2018-01-20 13:38:50 +0100

    Read .xyz point clouds and convert them to strokes in a naïve way.
    Refactoring to better associate importers for palettes, wireframes and point clouds with their corresponding stroke generation algorithm.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SharpQuill")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Joan Charmant 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
